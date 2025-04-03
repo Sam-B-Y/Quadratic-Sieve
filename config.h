96c03cd431f114c9e9c915ddeec44aa26190cfc3
Sam Borremans <samborremans.y@gmail.com>
2025-04-03 16:02:53 -0400

    Feat: checks for input and created config file

---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Maximum number of digits allowed for the composite number
+#define MAX_DIGITS 100
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <gmpxx.h>
+#include <gmpxx.h> // Use GMP library to handle large integers
+#include "config.h" // Global configuration file
 
 using namespace std;
 
 int main() {
     
     // Promt user for composite number n
-    // Use GMP library to handle large integers
 
     string nStr;
     cout << "Enter composite number n: ";
     cin >> nStr;
+
+    for (char c : nStr) { // Check if the input is a valid number
+        if (!isdigit(c)) {
+            cerr << "Error: Invalid input: '" << c << "'. Please enter a valid composite number." << endl;
+            return EXIT_FAILURE;
+        }
+    }
+
     mpz_class n(nStr); // n stores the composite number as a GMP integer
+
+    if(nStr.length() > MAX_DIGITS) { // Only proceed if the number of digits isn't too long
+        cerr << "Error: Number exceeds the maximum allowed digit limit of " << MAX_DIGITS << "." << endl;
+        return EXIT_FAILURE;
+    }
 
     cout << "Factoring composite number: " << n << endl;
 
+    return EXIT_SUCCESS;
 }
